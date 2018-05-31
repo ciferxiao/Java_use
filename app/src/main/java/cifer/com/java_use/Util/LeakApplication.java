44149fbef438372dcac5ef66ca3d3ef8412625a4
ciferxiao <1570058539@qq.com>
2018-05-31 16:39:05 +0800

    内存泄漏检测

---
+++ app/src/main/java/cifer/com/java_use/Util/LeakApplication.java
@@ -0,0 +1,36 @@
+package cifer.com.java_use.Util;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+
+/**
+ * Created by cifer
+ * on 2018/5/22 14:51.
+ * 内存泄漏检验
+ */
+
+public class LeakApplication extends Application {
+    private RefWatcher refWatcher;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        refWatcher= setupLeakCanary();
+    }
+    private RefWatcher setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return RefWatcher.DISABLED;
+        }
+        return LeakCanary.install(this);
+    }
+
+    public static RefWatcher getRefWatcher(Context context) {
+        LeakApplication leakApplication = (LeakApplication) context.getApplicationContext();
+        return leakApplication.refWatcher;
+    }
+
+
+}
